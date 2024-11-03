d08161d7b3da643237bd780bbe6b723e763cec0e
Lee <fw22912@bristol.ac.uk>
2024-11-03 11:12:59 +0000

    sceneloader

--- Assets/8/SceneLoader.cs
+++ Assets/8/SceneLoader.cs
@@ -1,25 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections;
 
 public class SceneLoader : MonoBehaviour
 {
-    public string nextSceneName; // Name of the next scene
-
-    private void OnTriggerEnter2D(Collider2D other)
+     public void LoadNextScene(string sceneName)
     {
-        if (other.CompareTag("Player"))
-        {
-            StartCoroutine(LoadNextSceneAsync());
-        }
-    }
-
-    private IEnumerator LoadNextSceneAsync()
-    {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextSceneName);
-        while (!asyncLoad.isDone)
-        {
-            yield return null; // Wait until the scene is fully loaded
-        }
+        SceneManager.LoadScene(sceneName);
     }
 }
